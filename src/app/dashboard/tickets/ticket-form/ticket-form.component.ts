5a8d3c5f6e73caabca20a3f79382cc37583c140d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-24 23:00:07 -0400

    Introduce reusable standalone `ControlComponent` and integrate into `TicketFormComponent`.

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-ticket-form',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    ControlComponent
   ],
   templateUrl: './ticket-form.component.html',
   styleUrl: './ticket-form.component.css'
