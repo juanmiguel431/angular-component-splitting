a3d4533f6ce0320c428886b7325df5404b12bb6e
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-24 21:28:07 -0400

    Introduce reusable standalone `ButtonComponent` and integrate into `HeaderComponent` and `TicketFormComponent`.

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-ticket-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './ticket-form.component.html',
   styleUrl: './ticket-form.component.css'
 })
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  public icon = input.required<string>();
+}
