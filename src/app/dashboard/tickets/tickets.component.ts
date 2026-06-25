da93421df96403667b93ad939e12d8f85bcd4735
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-24 21:13:31 -0400

    Add standalone `TicketFormComponent` and integrate into tickets dashboard

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TicketFormComponent } from './ticket-form/ticket-form.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [
+    TicketFormComponent
+  ],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
