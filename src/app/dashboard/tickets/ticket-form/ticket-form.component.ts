d89202161f8b48505252cb02f9a5a73225be959f
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-27 16:23:50 -0400

    Bind form inputs to component properties in `TicketFormComponent` and handle form submission via `ngSubmit`.

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-form',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './ticket-form.component.html',
   styleUrl: './ticket-form.component.css'
 })
 export class TicketFormComponent {
+  protected title?: string;
+  protected request?: string;
 
+  protected submit() {
+    console.log({ title: this.title, request: this.request })
+  }
 }
