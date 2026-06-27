78d65abf0cbd232699fe23b2a34c7acd1a68e5d3
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-27 17:48:35 -0400

    Refactor `TicketFormComponent` to use `viewChild` and `viewChildren` functions for `ViewChild` and `ViewChildren` bindings.

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ElementRef, QueryList, viewChild, ViewChild, viewChildren, ViewChildren } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -15,17 +15,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './ticket-form.component.css'
 })
 export class TicketFormComponent {
-  @ViewChild('form')
-  private form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form')
+  // private form?: ElementRef<HTMLFormElement>;
 
-  @ViewChildren(ControlComponent)
-  private controls?: QueryList<ControlComponent>;
+  // @ViewChildren(ControlComponent)
+  // private controls?: QueryList<ControlComponent>;
+
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  private controls = viewChildren(ControlComponent);
 
   protected submit(title: string, request: string) {
     console.log({ title: title, request: request });
 
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
 
-    console.log(this.controls?.toArray())
+    console.log(this.controls())
   }
 }
