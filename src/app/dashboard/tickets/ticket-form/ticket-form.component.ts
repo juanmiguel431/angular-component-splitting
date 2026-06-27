41c3629b58e9153b0fd0098a38b6fc56f798f371
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-27 17:20:19 -0400

    Add `ViewChildren` for retrieving `ControlComponent` instances in `TicketFormComponent`

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -18,9 +18,14 @@ export class TicketFormComponent {
   @ViewChild('form')
   private form?: ElementRef<HTMLFormElement>;
 
+  @ViewChildren(ControlComponent)
+  private controls?: QueryList<ControlComponent>;
+
   protected submit(title: string, request: string) {
     console.log({ title: title, request: request });
 
     this.form?.nativeElement.reset();
+
+    console.log(this.controls?.toArray())
   }
 }
