a215e236168efa772d46d8451ff801f8de63d303
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-27 21:20:44 -0400

    Add toggle logic and details visibility management to `TicketComponent` with Angular signals.

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Ticket } from '../../../models';
 
 @Component({
@@ -9,4 +9,10 @@ import { Ticket } from '../../../models';
 })
 export class TicketComponent {
   public item = input.required<Ticket>();
+
+  protected showDetails = signal(false);
+
+  protected toggleShowDetails() {
+    this.showDetails.update((value) => !value);
+  }
 }
